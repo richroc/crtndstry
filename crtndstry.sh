83690ce0a265baee9d79d5ee19a5a62d69babecd
Taffy <richroc@users.noreply.github.com>
2020-09-28 17:41:13 -0700

    Update crtndstry.sh

--- crtndstry.sh
+++ crtndstry.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # This was created during a live stream on 11/16/2019
 # twitch.tv/nahamsec
 # Thank you to nukedx and dmfroberson for helping debug/improve
@@ -16,10 +17,11 @@ digi()
 
 
 certdata(){
+	crtsh1=$(curl -s  "https://crt.sh/?q=$1&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee -a rawdata/crtsh1.txt || '')
+        echo "$crtsh1"
         #give it patterns to look for within crt.sh for example %api%.site.com
         declare -a arr=("api" "corp" "dev" "uat" "test" "stage" "sandbox" "prod" "internal")
-                crtsh1=$(curl -s  "https://crt.sh/?q=$1&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee -a rawdata/crtsh1.txt || '')
-	for i in "${arr[@]}"; do
+        for i in "${arr[@]}"; do
                 #get a list of domains based on our patterns in the array
                 sub="${i}.$1"
                 url="https://crt.sh/?q=${sub}&output=json"
@@ -32,7 +34,7 @@ certdata(){
                 #get a list of domains from digicert
                 #digicert=$(digi $1)
                 echo "$crtsh1"
-		echo "$crtsh"
+                echo "$crtsh"
                 echo "$certspotter"
                 #echo "$digicert"
 }
@@ -42,14 +44,18 @@ certdata(){
 rootdomains() { #this creates a list of all unique root sub domains
         clear
         echo "working on data"
-	cat rawdata/$1-crtsh1.txt | rev | cut -d "."  -f 1,2,3 | sort -u | rev | tee ./$1-temp.txt
-        cat rawdata/$1-crtsh.txt | rev | cut -d "."  -f 1,2,3 | sort -u | rev | tee -a ./$1-temp.txt
-        cat rawdata/$1-certspotter.txt | rev | cut -d "."  -f 1,2,3 | sort -u | rev | tee -a ./$1-temp.txt
+        cat rawdata/crtsh1.txt | rev | cut -d "." -f 1,2,3 | sort -u | rev | tee ./$1-temp.txt
+        cat rawdata/crtsh.txt | rev | cut -d "." -f 1,2,3 | sort -u | rev | tee -a ./$1-temp.txt
+        cat rawdata/certspotter.txt | rev | cut -d "." -f 1,2,3 | sort -u | rev | tee -a ./$1-temp.txt
         domain=$1
         #jq -r '.data.certificateDetail[].commonName,.data.certificateDetail[].subjectAlternativeNames[]' rawdata/digicert.json | sed 's/"//g' | grep -w "$domain$" | rev | cut -d "."  -f 1,2,3 | sort -u | rev | tee -a ./$1-temp.txt
         cat $1-temp.txt | sort -u | tee ./data/$1-$(date "+%Y.%m.%d-%H.%M").txt; rm $1-temp.txt
+        cat rawdata/*.txt >> ./data/$1-Full_Domains.txt
+        rm -rf rawdata/*.txt
         echo "Number of domains found: $(cat ./data/$1-$(date "+%Y.%m.%d-%H.%M").txt | wc -l)"
 }
 
 certdata $1
 rootdomains $1
+echo "All Unique root sub domains: $(cat ./data/$1-$(date "+%Y.%m.%d-%H.%M").txt | wc -l)"
+echo "All Unique domains: $(cat ./data/$1-Full_Domains.txt | wc -l)"
